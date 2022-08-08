46190c7b77ee9c5f14c13c2180d3ec142b5d6b58
Luis Migryk <luis.migryk@usach.cl>
2022-08-07 21:00:56 -0400

    feat: commit with initial structure

---
+++ src/test/java/com/example/meli/MeliTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
