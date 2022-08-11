3f71c06a71166b64df94df667d7810ee12dbb0f8
Luis Migryk <luis.migryk@usach.cl>
2022-08-11 11:21:42 -0400

    feat: add unit Test complete

---
+++ src/main/java/com/example/meli/domain/services/PiServiceImp.java
@@ -0,0 +1,70 @@
+package com.example.meli.domain.services;
+
+import com.example.meli.commons.dto.DtoResponseDelete;
+import com.example.meli.commons.exception.ApiExceptionBase;
+import com.example.meli.commons.exception.DeleteException;
+import com.example.meli.commons.utils.SerialPi;
+import com.example.meli.commons.validator.RandomConstraint;
+import com.example.meli.domain.models.BasePi;
+import lombok.SneakyThrows;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Min;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+@Validated
+@Service
+public class PiService implements IPiService{
+    private final CacheManager cacheManager;
+    private final DtoResponseDelete = 
+    private final
+    public PiService(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+    @Override
+    @Cacheable(cacheNames = "number_pi", key = "#random" ,unless = "#result == null")
+    public BasePi getPiRandom(int random) {
+        System.out.println("No estaba creado " + random + " se creará ");
+        BasePi numberPi =  calculatedPi(random);
+        return numberPi;
+    }
+    @Override
+    @Cacheable(cacheNames = "number_pi", key = "#numberUSer" ,unless = "#result == null")
+    public BasePi getPiNotRandom(int numberUSer) {
+        System.out.println("No estaba creado " + numberUSer + " se creará ");
+        return calculatedPi(numberUSer);
+    }
+
+    @Override
+    @SneakyThrows
+    public String deletePi(int numberUSer)  {
+        if (cacheManager.getCache("number_pi").get(numberUSer) != null) {
+            System.out.println("si vez este mensaje es por q se elimino  " + numberUSer);
+            cacheManager.getCache("number_pi").evict(numberUSer);
+        }
+        else{
+            throw new DeleteException("Not found number key in cache redis","DONT_DELETE","https://httpstatuses.com/409");
+        }
+
+    }
+    public BasePi calculatedPi( int decimales){
+        SerialPi serie = new SerialPi(decimales);
+        int incremento = 0;
+        BigDecimal sumatoria =  new BigDecimal(0), anteriorSumatoria;
+        boolean variable = true;
+        while (variable){
+            anteriorSumatoria = sumatoria;
+            sumatoria = sumatoria.add(serie.calculateIteration(incremento));
+            incremento +=1;
+            variable = !sumatoria.setScale(decimales, RoundingMode.FLOOR).equals(anteriorSumatoria.setScale(decimales, RoundingMode.FLOOR));
+        }
+        BasePi result = new BasePi(decimales,sumatoria.setScale(decimales, RoundingMode.FLOOR).toString());
+
+        return result;
+
+    }
+
+}
